291f0040401c641be2df31f134cea4400e417248
David Schall <d.h.schall@sms.ed.ac.uk>
2022-03-28 08:56:49 -0600

    Stop scripts upon failing

--- setup/build_gem5.sh
+++ setup/build_gem5.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+# MIT License
+#
+# Copyright (c) 2022 David Schall and EASE lab
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# Install dependencies
+
+set -e -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT="$( cd $DIR && cd .. && pwd)"
+
+
 ARCH=X86
-GEM5_DIR=$HOME/gem5/
+GEM5_DIR=${1:-../gem5/}
 
 # Setup on Ubuntu 20.04
 sudo apt update
@@ -32,7 +61,9 @@ pushd $GEM5_DIR/util/m5
 scons build/$ARCH/out/m5
 popd
 
-export GEM5_DIR=$PWD/$GEM5_DIR
+# Export absolute path
+GEM5_DIR="$( cd $GEM5_DIR && pwd)"
+export GEM5_DIR=$GEM5_DIR
 
 sudo sh -c  "echo 'export GEM5_DIR=${GEM5_DIR}' >> /etc/profile"
 sh -c  "echo 'export GEM5_DIR=${GEM5_DIR}' >> $HOME/.bashrc"
--- setup/build_kernel.sh
+++ setup/build_kernel.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
+# MIT License
+#
+# Copyright (c) 2022 David Schall and EASE lab
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# Install dependencies
+
+set -e -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT="$( cd $DIR && cd .. && pwd)"
+
 
 ### Build kernel for gem5 supporting running docker images
 KVERSION=5.4.84
-TARGET_DIR=../workload/
+OUTDIR=${1:-../workload/}
 
-git build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
-sudo apt-get install -y libncurses5-dev \
-                        gcc make git exuberant-ctags bc libssl-dev
+## Install dependencies
+sudo apt-get install -y \
+    git build-essential ncurses-dev xz-utils libssl-dev bc \
+    flex libelf-dev bison
 
 # Get sources
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git linux
@@ -15,14 +44,14 @@ pushd linux
 git checkout v${KVERSION}
 
 # Apply the configuration
-cp ../configs/linux-${KVERSION}.config .config
+cp ${ROOT}/configs/linux-${KVERSION}.config .config
 
 ## build kernel
 make -j $(nproc)
 
 ## place in ouput folder
-mkdir -p $TARGET_DIR
-cp vmlinux $TARGET_DIR
+mkdir -p $OUTDIR
+cp vmlinux $OUTDIR
 popd
 
 ## Clean up the linux sources
