460d7bbd6feb28ed638b7ec61f41698483a1d8a4
David Schall <d.h.schall@sms.ed.ac.uk>
2022-05-16 13:53:19 -0600

    Add m5ops to client
    Signed-off-by: David Schall <d.h.schall@sms.ed.ac.uk>

---
+++ tools/client/m5/m5ops.go
@@ -0,0 +1,108 @@
+package m5ops
+
+/*
+#cgo CFLAGS: -g
+#cgo LDFLAGS: -L. -lm5ops
+#include "m5ops.h"
+#include "m5_mmap.h"
+*/
+import "C"
+import (
+	"os/user"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type M5Ops struct {
+	// init bool
+	mapped bool
+}
+
+func NewM5Ops() M5Ops {
+	op := M5Ops{}
+	op.init()
+	return op
+}
+
+func isRoot() bool {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Warn("Cannot get current user")
+	}
+	return currentUser.Uid == "0"
+}
+
+func (op *M5Ops) init() {
+	if !isRoot() {
+		log.Warn("M5 magic instructions require root access")
+		log.Warn("Will not be enabled!!!")
+		return
+	}
+	log.Println("Initialize M5 magic instructions")
+	C.map_m5_mem()
+	op.mapped = true
+}
+
+func (op *M5Ops) Close() {
+	if op.mapped {
+		C.unmap_m5_mem()
+		op.mapped = false
+	}
+}
+
+func (op M5Ops) Exit(delay int) {
+	if !op.mapped {
+		log.Warn("M5 disabled")
+		return
+	}
+	C.m5_exit_addr(C.ulong(delay))
+}
+
+func (op M5Ops) Fail(delay, code int) {
+	if !op.mapped {
+		log.Warn("M5 disabled")
+		return
+	}
+	C.m5_fail_addr(C.ulong(delay), C.ulong(code))
+}
+
+func (op M5Ops) DumpStats(delay, period int) {
+	if !op.mapped {
+		return
+	}
+	C.m5_dump_stats_addr(C.ulong(delay), C.ulong(period))
+}
+
+func (op M5Ops) ResetStats(delay, period int) {
+	if !op.mapped {
+		return
+	}
+	C.m5_reset_stats_addr(C.ulong(delay), C.ulong(period))
+}
+
+func (op M5Ops) DumpResetStats(delay, period int) {
+	if !op.mapped {
+		return
+	}
+	C.m5_dump_reset_stats_addr(C.ulong(delay), C.ulong(period))
+}
+func (op M5Ops) WorkBegin(workid, threadid int) {
+	if !op.mapped {
+		return
+	}
+	C.m5_work_begin_addr(C.ulong(workid), C.ulong(threadid))
+}
+
+func (op M5Ops) WorkEnd(workid, threadid int) {
+	if !op.mapped {
+		return
+	}
+	C.m5_work_end_addr(C.ulong(workid), C.ulong(threadid))
+}
+
+func (op M5Ops) Workload() {
+	if !op.mapped {
+		return
+	}
+	C.m5_workload_addr()
+}
