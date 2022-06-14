8562e9144c95899804b35e88a82b1d791877a41e
David Schall <david.schall@ed.ac.uk>
2022-06-14 15:30:30 -0600

    Only the gem5 runner is used at the moment.
    Signed-off-by: David Schall <d.h.schall@sms.ed.ac.uk>

--- runner/easy-create.sh
+++ runner/easy-create.sh
@@ -22,20 +22,28 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-set -e -x
+# Execute this script (on the local) as
+#   GH_ACCESS_TOKEN=... ./easy-create.sh <HOSTNAME> <N> [USER]
+#
+
+set -x
+set -e
 
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 REMOTE=$1
-AS_USER=$2
-PRIVATE_KEY=$3
+COUNT=$2
+AS_USER=${3:-`whoami`}
 
 sudo apt update -qq
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -q ansible
 
-
 ansible-playbook -v -u $AS_USER -i ${REMOTE}, ${PWD}/setup-host.yaml
 
-GH_ACCESS_TOKEN=${GH_ACCESS_TOKEN} ansible-playbook -v -u $AS_USER -i ${REMOTE}, ${PWD}/create-runner.yaml
+# ansible-playbook -u $AS_USER -i ${REMOTE}, ${PWD}/delete-runners.yaml
+
+for i in $(seq ${COUNT}); do
+    GH_ACCESS_TOKEN=${GH_ACCESS_TOKEN} ansible-playbook -u $AS_USER -i ${REMOTE}, ${PWD}/create-runner.yaml
+done
